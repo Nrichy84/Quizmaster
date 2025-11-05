12133de39e9b09af996fdcdc3227f1e761ba5276
Nrichy84 <nrichy2@gmail.com>
2025-11-05 23:27:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
