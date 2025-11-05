91dd26d38fd3042701d9ae5363d96f590e4b0083
Nrichy84 <nrichy2@gmail.com>
2025-11-05 22:54:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '..', 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
 app.set('layout', 'layout');
 
